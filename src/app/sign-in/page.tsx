3cafaa87572a31da0c2f306b0cfb69dbbacd7ab7
Shostyy <anatoliy.shostal@gmail.com>
2025-01-30 22:36:07 +0200

    Implement basic google auth. Create SignIn page.

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleAuthProvider } from "../../../firabase-config";
+import { useRouter } from "next/navigation";
+
+export default function SignIn() {
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      await signInWithPopup(auth, googleAuthProvider);
+      router.push("/");
+    } catch {
+      console.error("Login failed");
+    }
+  };
+
+  return (
+    <>
+      <button onClick={handleLogin}>login with google</button>
+    </>
+  );
+}
